c91d37f4268889dd35e5ec5557c087fe3301502f
Alejandro <correa92.dev@gmail.com>
2025-03-24 20:24:21 -0300

    se pone en funcionamiento score

--- src/model/service/ScoreService.java
+++ src/model/service/ScoreService.java
@@ -6,13 +6,27 @@ import java.util.List;
 import model.entity.Score;
 
 public class ScoreService {
+	
     private List<Score> scoreList = new ArrayList<>();
-
-    public void addScore(String playerName, int points) {
+    private Score currentUser = new Score("Jugador", 0); 
+    
+    public void addScoreAndUser(String playerName, int points) {
         scoreList.add(new Score(playerName, points));
+    }
+    
+    public void addPoint() {
+    	currentUser.setPoints();
+    }
+    
+    public int getScore() {
+    	return currentUser.getPoints();
     }
 
     public List<Score> getScores() {
         return scoreList;
+    }
+    
+    public void resetScore() {
+    	currentUser.setPoints(0);
     }
 }
